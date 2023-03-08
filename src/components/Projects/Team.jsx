be35eea1d8f0c253e2e3fa14086ef374d6ab6446
Bhimendra Nerli <bhimendran@birlasoft.com>
2023-03-08 18:12:33 +0530

    changed in teams

--- src/components/Projects/Team.jsx
+++ src/components/Projects/Team.jsx
@@ -1,77 +1,93 @@
-import axios from "axios"
-import React, { useEffect } from "react"
-import { useState } from "react"
-import { useSelector } from "react-redux"
-import CircularProgress from '@mui/material/CircularProgress';
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import CircularProgress from "@mui/material/CircularProgress";
 import { Button } from "@mui/material";
 
-
 export const Team = () => {
-  const user = JSON.parse(localStorage.getItem("okta-token-storage"))
-  const projectdetails = useSelector((state) => state.projectReducers.projectDetails)
-  const id = projectdetails.Id
-  const token = user?.accessToken?.accessToken
-  const [teamUrl, setTeamUrl] = useState('')
-  const [emptyTeamUrlMsg, setEmptyTeamUrlMsg] = useState(false)
-  const [loadingTeamUrl, setLoadingTeamUrl] = useState(false)
-
+  const user = JSON.parse(localStorage.getItem("okta-token-storage"));
+  const projectdetails = useSelector(
+    (state) => state.projectReducers.projectDetails
+  );
+  const id = projectdetails.Id;
+  const token = user?.accessToken?.accessToken;
+  const [teamUrl, setTeamUrl] = useState("");
+  const [emptyTeamUrlMsg, setEmptyTeamUrlMsg] = useState(false);
+  const [loadingTeamUrl, setLoadingTeamUrl] = useState(false);
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
   const getData = async () => {
-    setLoadingTeamUrl(true)
-    const response = await axios.get(`${process.env.REACT_APP_VIS_SERVICE_URL}/projects/${id}/team/info`, {
-      headers: {
-        "Authorization": `Bearer ${token}`
+    setLoadingTeamUrl(true);
+    const response = await axios.get(
+      `${process.env.REACT_APP_VIS_SERVICE_URL}/projects/${id}/team/info`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
-    })
-    // console.log(response)
+    );
+    console.log("get==", response.data.url);
     if (response.status === 200) {
-      setLoadingTeamUrl(false)
-      setTeamUrl(response.data.url)
+      setLoadingTeamUrl(false);
+      setTeamUrl(response.data.url);
     } else {
-      setLoadingTeamUrl(false)
+      setLoadingTeamUrl(false);
     }
-    if (response.data === 'Error Occured') {
-      setEmptyTeamUrlMsg(true)
+    if (response.data === "Error Occured") {
+      setEmptyTeamUrlMsg(true);
     }
-  }
+  };
 
   // console.log('ress--', teamUrl)
 
-  const [teamImageUrl, setTeamImageUrl] = useState()
+  const [teamImageUrl, setTeamImageUrl] = useState();
 
   const handleAssignTeamUrl = async () => {
     // const payload = {
     //   teams: teamImageUrl?.team
     // }
-    var formData = new FormData()
-    formData.append('teamImageUrl', teamImageUrl)
-    const result = await axios.post(`${process.env.REACT_APP_VIS_SERVICE_URL}/projects/${id}/team/info`, formData, {
-      headers: {
-        "Authorization": `Bearer ${token}`
+    var formData = new FormData();
+    formData.append("file", teamImageUrl);
+    const result = await axios.post(
+      `${process.env.REACT_APP_VIS_SERVICE_URL}/projects/${id}/team/info`,
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
-    })
-    console.log(result.data)
+    );
+    console.log(result);
     if (result.status === 200) {
-      getData()
+      getData();
     }
-  }
-
+  };
 
   return (
     <div style={{ marginTop: "3px" }}>
+      <input
+        type="file"
+        accept="teamImageUrl/*"
+        onChange={(e) => setTeamImageUrl(e.target.files[0])}
+      />
+      <Button variant="contained" onClick={handleAssignTeamUrl}>
+        add
+      </Button>
 
-      <input type="file" accept="teamImageUrl/*" onChange={(e) => setTeamImageUrl(e.target.files[0])} />
-      <Button variant="contained" onClick={handleAssignTeamUrl}>add</Button>
-
-      {emptyTeamUrlMsg ? "No image available" :
+      {emptyTeamUrlMsg ? (
+        "No image available"
+      ) : (
         <div>
-          {loadingTeamUrl ? <CircularProgress color="success" /> :
-            <img height={'800px'} width={'1024px'} alt="" src={teamUrl} />}
-        </div>}
+          {loadingTeamUrl ? (
+            <CircularProgress color="success" />
+          ) : (
+            <img height={"800px"} width={"1024px"} alt="" src={teamUrl} />
+          )}
+        </div>
+      )}
     </div>
-
-  )
-}
+  );
+};
